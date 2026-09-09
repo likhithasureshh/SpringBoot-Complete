3db17b732a1a07e42a7f2de614751a2c182969d7
likhithasureshh <likithaa.sureshh@gmail.com>
2026-09-09 19:43:58 +0530

    m-5 5.5

--- module-5/prod-ready-features/src/test/java/com/springboot/prod_ready_features/ProdReadyFeaturesApplicationTests.java
+++ module-5/prod-ready-features/src/test/java/com/springboot/prod_ready_features/ProdReadyFeaturesApplicationTests.java
@@ -1,13 +1,25 @@
 package com.springboot.prod_ready_features;
 
+import com.springboot.prod_ready_features.entities.User;
+import com.springboot.prod_ready_features.service.impl.JwtService;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class ProdReadyFeaturesApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+	@Autowired
+	private JwtService jwtService;
 
+	@Test
+	void generateToken()
+	{
+		User user = new User(4L,"liki@gmail.com","123");
+
+		String token = jwtService.generateToken(user);
+		System.out.println(token);
+
+		System.out.println(jwtService.getUserId(token));
+	}
 }
